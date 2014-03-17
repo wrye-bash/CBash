f853afa7bd6d9bf31e4d6a1b66143fe5e0adeade
lojack5 <lojo.jacob@gmail.com>
2014-03-17 13:42:12 -0600

    Fix Compile errors: FNV:IPDSRecord

--- FalloutNewVegas/Records/IPDSRecord.cpp
+++ FalloutNewVegas/Records/IPDSRecord.cpp
@@ -92,7 +92,7 @@ STRING IPDSRecord::GetStrType()
     return "IPDS";
     }
 
-SINT32 IPDSRecord::ParseRecord(unsigned char *buffer, const UINT32 &recSize)
+SINT32 IPDSRecord::ParseRecord(unsigned char *buffer, unsigned char *end_buffer, bool CompressedOnDisk)
     {
     UINT32 subType = 0;
     UINT32 subSize = 0;
@@ -116,7 +116,7 @@ SINT32 IPDSRecord::ParseRecord(unsigned char *buffer, const UINT32 &recSize)
         switch(subType)
             {
             case REV32(EDID):
-                EDID.Read(buffer, subSize);
+                EDID.Read(buffer, subSize, CompressedOnDisk);
                 break;
             case REV32(DATA):
                 DATA.Read(buffer, subSize);
@@ -161,7 +161,7 @@ bool IPDSRecord::operator !=(const IPDSRecord &other) const
     return !(*this == other);
     }
 
-bool IPDSRecord::equals(const Record *other) const
+bool IPDSRecord::equals(Record *other)
     {
     return *this == *(IPDSRecord *)other;
     }
