0dab9718ba0d2338ab9832ffcfd41e8772a1a675
lojack5 <lojo.jacob@gmail.com>
2014-03-22 12:08:36 -0600

    Fix Compile errors: FNV:LSCTRecordAPI

--- FalloutNewVegas/Records/API/LSCTRecordAPI.cpp
+++ FalloutNewVegas/Records/API/LSCTRecordAPI.cpp
@@ -153,43 +153,43 @@ void * LSCTRecord::GetField(FIELD_IDENTIFIERS, void **FieldValues)
             *FieldValues = &versionControl2[0];
             return NULL;
         case 7: //data DATA ,, Struct
-            return DATA.IsLoaded() ? &DATA->value7 : NULL;
+            return DATA.IsLoaded() ? &DATA->screenType : NULL;
         case 8: //data DATA ,, Struct
-            return DATA.IsLoaded() ? &DATA->value8 : NULL;
+            return DATA.IsLoaded() ? &DATA->x : NULL;
         case 9: //data DATA ,, Struct
-            return DATA.IsLoaded() ? &DATA->value9 : NULL;
+            return DATA.IsLoaded() ? &DATA->y : NULL;
         case 10: //data DATA ,, Struct
-            return DATA.IsLoaded() ? &DATA->value10 : NULL;
+            return DATA.IsLoaded() ? &DATA->width : NULL;
         case 11: //data DATA ,, Struct
-            return DATA.IsLoaded() ? &DATA->value11 : NULL;
+            return DATA.IsLoaded() ? &DATA->height : NULL;
         case 12: //data DATA ,, Struct
-            return DATA.IsLoaded() ? &DATA->value12 : NULL;
+            return DATA.IsLoaded() ? &DATA->orientation : NULL;
         case 13: //data DATA ,, Struct
-            return DATA.IsLoaded() ? &DATA->value13 : NULL;
+            return DATA.IsLoaded() ? &DATA->font1 : NULL;
         case 14: //data DATA ,, Struct
-            return DATA.IsLoaded() ? &DATA->value14 : NULL;
+            return DATA.IsLoaded() ? &DATA->font1Red : NULL;
         case 15: //data DATA ,, Struct
-            return DATA.IsLoaded() ? &DATA->value15 : NULL;
+            return DATA.IsLoaded() ? &DATA->font1Green : NULL;
         case 16: //data DATA ,, Struct
-            return DATA.IsLoaded() ? &DATA->value16 : NULL;
+            return DATA.IsLoaded() ? &DATA->font1Blue : NULL;
         case 17: //data DATA ,, Struct
-            return DATA.IsLoaded() ? &DATA->value17 : NULL;
+            return DATA.IsLoaded() ? &DATA->alignmentType : NULL;
         case 18: //data_p DATA ,, Struct
-            *FieldValues = DATA.IsLoaded() ? &DATA->value18[0] : NULL;
+            *FieldValues = DATA.IsLoaded() ? &DATA->unknown1[0] : NULL;
             return NULL;
         case 19: //data DATA ,, Struct
-            return DATA.IsLoaded() ? &DATA->value19 : NULL;
+            return DATA.IsLoaded() ? &DATA->font2 : NULL;
         case 20: //data DATA ,, Struct
-            return DATA.IsLoaded() ? &DATA->value20 : NULL;
+            return DATA.IsLoaded() ? &DATA->font2Red : NULL;
         case 21: //data DATA ,, Struct
-            return DATA.IsLoaded() ? &DATA->value21 : NULL;
+            return DATA.IsLoaded() ? &DATA->font2Green : NULL;
         case 22: //data DATA ,, Struct
-            return DATA.IsLoaded() ? &DATA->value22 : NULL;
+            return DATA.IsLoaded() ? &DATA->font2Blue : NULL;
         case 23: //data_p DATA ,, Struct
-            *FieldValues = DATA.IsLoaded() ? &DATA->value23[0] : NULL;
+            *FieldValues = DATA.IsLoaded() ? &DATA->unknown2[0] : NULL;
             return NULL;
         case 24: //data DATA ,, Struct
-            return DATA.IsLoaded() ? &DATA->value24 : NULL;
+            return DATA.IsLoaded() ? &DATA->stats : NULL;
         default:
             return NULL;
         }
@@ -225,101 +225,83 @@ bool LSCTRecord::SetField(FIELD_IDENTIFIERS, void *FieldValue, UINT32 ArraySize)
             break;
         case 7: //data DATA ,, Struct
             DATA.Load();
-            DATA->value7 = *(UINT32 *)FieldValue;
+            DATA->screenType = *(UINT32 *)FieldValue;
             break;
         case 8: //data DATA ,, Struct
             DATA.Load();
-            DATA->value8 = *(UINT32 *)FieldValue;
+            DATA->x = *(UINT32 *)FieldValue;
             break;
         case 9: //data DATA ,, Struct
             DATA.Load();
-            DATA->value9 = *(UINT32 *)FieldValue;
+            DATA->y = *(UINT32 *)FieldValue;
             break;
         case 10: //data DATA ,, Struct
             DATA.Load();
-            DATA->value10 = *(UINT32 *)FieldValue;
+            DATA->width = *(UINT32 *)FieldValue;
             break;
         case 11: //data DATA ,, Struct
             DATA.Load();
-            DATA->value11 = *(UINT32 *)FieldValue;
+            DATA->height = *(UINT32 *)FieldValue;
             break;
         case 12: //data DATA ,, Struct
             DATA.Load();
-            DATA->value12 = *(FLOAT32 *)FieldValue;
+            DATA->orientation = *(FLOAT32 *)FieldValue;
             break;
         case 13: //data DATA ,, Struct
             DATA.Load();
-            DATA->value13 = *(UINT32 *)FieldValue;
+            DATA->font1 = *(UINT32 *)FieldValue;
             break;
         case 14: //data DATA ,, Struct
             DATA.Load();
-            DATA->value14 = *(FLOAT32 *)FieldValue;
+            DATA->font1Red = *(FLOAT32 *)FieldValue;
             break;
         case 15: //data DATA ,, Struct
             DATA.Load();
-            DATA->value15 = *(FLOAT32 *)FieldValue;
+            DATA->font1Green = *(FLOAT32 *)FieldValue;
             break;
         case 16: //data DATA ,, Struct
             DATA.Load();
-            DATA->value16 = *(FLOAT32 *)FieldValue;
+            DATA->font1Blue = *(FLOAT32 *)FieldValue;
             break;
         case 17: //data DATA ,, Struct
             DATA.Load();
-            DATA->value17 = *(UINT32 *)FieldValue;
+            DATA->alignmentType = *(UINT32 *)FieldValue;
             break;
         case 18: //data_p DATA ,, Struct
             if(ArraySize != 20)
                 break;
             DATA.Load();
-            DATA->value18[0] = ((UINT8ARRAY)FieldValue)[0];
-            DATA->value18[1] = ((UINT8ARRAY)FieldValue)[1];
-            DATA->value18[2] = ((UINT8ARRAY)FieldValue)[2];
-            DATA->value18[3] = ((UINT8ARRAY)FieldValue)[3];
-            DATA->value18[4] = ((UINT8ARRAY)FieldValue)[4];
-            DATA->value18[5] = ((UINT8ARRAY)FieldValue)[5];
-            DATA->value18[6] = ((UINT8ARRAY)FieldValue)[6];
-            DATA->value18[7] = ((UINT8ARRAY)FieldValue)[7];
-            DATA->value18[8] = ((UINT8ARRAY)FieldValue)[8];
-            DATA->value18[9] = ((UINT8ARRAY)FieldValue)[9];
-            DATA->value18[10] = ((UINT8ARRAY)FieldValue)[10];
-            DATA->value18[11] = ((UINT8ARRAY)FieldValue)[11];
-            DATA->value18[12] = ((UINT8ARRAY)FieldValue)[12];
-            DATA->value18[13] = ((UINT8ARRAY)FieldValue)[13];
-            DATA->value18[14] = ((UINT8ARRAY)FieldValue)[14];
-            DATA->value18[15] = ((UINT8ARRAY)FieldValue)[15];
-            DATA->value18[16] = ((UINT8ARRAY)FieldValue)[16];
-            DATA->value18[17] = ((UINT8ARRAY)FieldValue)[17];
-            DATA->value18[18] = ((UINT8ARRAY)FieldValue)[18];
-            DATA->value18[19] = ((UINT8ARRAY)FieldValue)[19];
+            for(int i = 0; i < 20; ++i)
+                DATA->unknown1[i] = ((UINT8ARRAY)FieldValue)[i];
             break;
         case 19: //data DATA ,, Struct
             DATA.Load();
-            DATA->value19 = *(UINT32 *)FieldValue;
+            DATA->font2 = *(UINT32 *)FieldValue;
             break;
         case 20: //data DATA ,, Struct
             DATA.Load();
-            DATA->value20 = *(FLOAT32 *)FieldValue;
+            DATA->font2Red = *(FLOAT32 *)FieldValue;
             break;
         case 21: //data DATA ,, Struct
             DATA.Load();
-            DATA->value21 = *(FLOAT32 *)FieldValue;
+            DATA->font2Green = *(FLOAT32 *)FieldValue;
             break;
         case 22: //data DATA ,, Struct
             DATA.Load();
-            DATA->value22 = *(FLOAT32 *)FieldValue;
+            DATA->font2Blue = *(FLOAT32 *)FieldValue;
             break;
         case 23: //data_p DATA ,, Struct
             if(ArraySize != 4)
                 break;
             DATA.Load();
-            DATA->value23[0] = ((UINT8ARRAY)FieldValue)[0];
-            DATA->value23[1] = ((UINT8ARRAY)FieldValue)[1];
-            DATA->value23[2] = ((UINT8ARRAY)FieldValue)[2];
-            DATA->value23[3] = ((UINT8ARRAY)FieldValue)[3];
+            DATA->unknown2[0] = ((UINT8ARRAY)FieldValue)[0];
+            DATA->unknown2[1] = ((UINT8ARRAY)FieldValue)[1];
+            DATA->unknown2[2] = ((UINT8ARRAY)FieldValue)[2];
+            DATA->unknown2[3] = ((UINT8ARRAY)FieldValue)[3];
             break;
         case 24: //data DATA ,, Struct
             DATA.Load();
-            DATA->value24 = *(UINT32 *)FieldValue;
+            DATA->stats = *(UINT32 *)FieldValue;
             break;
         default:
             break;
