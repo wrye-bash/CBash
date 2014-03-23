68c69f9a7d662733df05ae85e9895630642cf9c5
lojack5 <lojo.jacob@gmail.com>
2014-03-23 15:38:19 -0600

    Fix Link errors: FNV:LSCTRecord

--- FalloutNewVegas/Records/LSCTRecord.cpp
+++ FalloutNewVegas/Records/LSCTRecord.cpp
@@ -38,6 +38,54 @@
 
 namespace FNV
 {
+LSCTRecord::LSCTDATA::LSCTDATA():
+    screenType(eNone),
+    x(0), y(0), width(0), height(0),
+    orientation(0.0),
+    font1(0),
+    font1Red(0.0), font1Green(0.0), font1Blue(0.0),
+    alignmentType(0),
+    font2(0),
+    font2Red(0.0), font2Green(0.0), font2Blue(0.0),
+    stats(0)
+    {
+    memset(&unknown1, 0, sizeof(unknown1));
+    memset(&unknown2, 0, sizeof(unknown2));
+    }
+
+LSCTRecord::LSCTDATA::~LSCTDATA()
+    {
+    //
+    }
+
+bool LSCTRecord::LSCTDATA::operator ==(const LSCTDATA &other) const
+    {
+    return (screenType == other.screenType &&
+            x == other.x &&
+            y == other.y &&
+            width == other.width &&
+            height == other.height &&
+            AlmostEqual(orientation, other.orientation, 2) &&
+            font1 == other.font1 &&
+            AlmostEqual(font1Red, other.font1Red, 2) &&
+            AlmostEqual(font1Green, other.font1Green, 2) &&
+            AlmostEqual(font1Blue, other.font1Blue, 2) &&
+            alignmentType == other.alignmentType &&
+            memcmp(&unknown1, &other.unknown1, sizeof(unknown1)) == 0 &&
+            font2 == other.font2 &&
+            AlmostEqual(font2Red, other.font2Red, 2) &&
+            AlmostEqual(font2Green, other.font2Green, 2) &&
+            AlmostEqual(font2Blue, other.font2Blue, 2) &&
+            memcmp(&unknown2, &other.unknown2, sizeof(unknown2)) == 0 &&
+            stats == other.stats
+            );
+    }
+
+bool LSCTRecord::LSCTDATA::operator !=(const LSCTDATA &other) const
+    {
+    return !(*this == other);
+    }
+
 LSCTRecord::LSCTRecord(unsigned char *_recData):
     FNVRecord(_recData)
     {
