c5fb6758f7acc212820aa2977d2a54caf4e5dbec
leandor <leandor@gmail.com>
2016-11-02 03:08:44 -0300

    (#6) Split the file `src/GRUPRecord.h`
    Split the content of the file `src/GRUPRecord.h` into three pieces and added
    each corresponding part to the existing `src/game/<game>/GRUPRecord.h` that
    was already in existance.
    
    After that the original file was empty, so it was deleted.
    
    Also the `src/game/<game>/GRUPRecord.h` files now required explicit inclusion
    of `Allocator.h` since the content that was moved to them depended on the
    class template declared there.

--- src/game/Skyrim/GRUPRecord.h
+++ src/game/Skyrim/GRUPRecord.h
@@ -34,9 +34,166 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
+#include "common/Allocator.h"
 #include "Records/CELLRecord.h"
 #include "Records/LANDRecord.h"
 #include "Records/WRLDRecord.h"
+
+template<class T, uint32_t RecType, uint32_t AllocUnit, bool IsKeyedByEditorID=false>
+class TES5GRUPRecords
+    {
+    public:
+        RecordPoolAllocator<T, RecType, AllocUnit> pool;
+        uint32_t stamp, unknown;
+
+        TES5GRUPRecords():
+            stamp(134671),
+            unknown(0)
+        {
+        //
+        }
+
+        ~TES5GRUPRecords()
+        {
+        //
+        }
+
+        bool Read(unsigned char *&buffer_start, unsigned char *&buffer_position, unsigned char *&group_buffer_end, RecordOp &indexer, RecordOp &parser, std::vector<Record *> &DeletedRecords, RecordProcessor &processor, char * &FileName)
+        {
+            stamp = *(uint32_t *)buffer_position;
+            buffer_position += 4;
+            unknown = *(uint32_t *)buffer_position;
+            buffer_position += 4;
+            if(group_buffer_end <= buffer_position)
+            {
+                printer("TES5GRUPRecords::Read: Error - Unable to load group in file \"%s\". The group has a size of 0.\n", FileName);
+#ifdef CBASH_DEBUG_CHUNK
+                peek_around(buffer_position, PEEK_SIZE);
+#endif
+                return false;
+            }
+
+            Record * curRecord = NULL;
+            uint32_t recordSize = 0;
+            RecordHeader header;
+
+            std::vector<RecordHeader> records;
+            records.reserve((uint32_t)(group_buffer_end - buffer_position) / sizeof(T)); //gross overestimation, but good enough
+            while(buffer_position < group_buffer_end)
+            {
+                if ((processor.IsSkipAllRecords && processor.IsTrackNewTypes) &&
+                    processor.NewTypes.count(RecType) > 0)
+                {
+                    buffer_position = group_buffer_end;
+                    break;
+                }
+
+                //Assumes that all records in a generic group are of the same type
+                header.type = *(uint32_t *)buffer_position;
+                buffer_position += 4;
+                recordSize = *(uint32_t *)buffer_position;
+                buffer_position += 4;
+                header.flags = *(uint32_t *)buffer_position;
+                buffer_position += 4;
+                header.formID = *(FORMID *)buffer_position;
+                buffer_position += 4;
+                header.flagsUnk = *(uint32_t *)buffer_position; //VersionControl1
+                buffer_position += 4;
+                header.formVersion = *(uint16_t *)buffer_position;
+                buffer_position += 2;
+                header.versionControl2[0] = *(uint8_t *)buffer_position;
+                buffer_position++;
+                header.versionControl2[1] = *(uint8_t *)buffer_position;
+                buffer_position++;
+
+                if (processor.Accept(header))
+                {
+                    header.data = buffer_position;
+                    records.push_back(header);
+                }
+
+                buffer_position += recordSize;
+            };
+
+            if (records.size())
+            {
+                //Allocates many records at once in a contiguous space
+                //Allocate memory
+                unsigned char *buffer = (unsigned char *)malloc(sizeof(T) * (uint32_t)records.size());
+                if (buffer == 0)
+                    throw std::bad_alloc();
+                pool.add_buffer(buffer);
+
+                //Construct the records
+                for(uint32_t x = 0; x < records.size();++x)
+                {
+                    header = records[x];
+                    curRecord = new(buffer) T(header.data);
+                    buffer += sizeof(T);
+                    curRecord->SetParent(processor.curModFile, true);
+                    curRecord->flags = header.flags;
+                    curRecord->formID = header.formID;
+                    curRecord->flagsUnk = header.flagsUnk; //VersionControl1
+                    ((TES5Record *)curRecord)->formVersion = header.formVersion;
+                    ((TES5Record *)curRecord)->versionControl2[0] = header.versionControl2[0];
+                    ((TES5Record *)curRecord)->versionControl2[1] = header.versionControl2[1];
+
+                    //Testing Messages
+                    //if((flags & 0x4000) != 0)
+                    //    printer("0x4000 used: %08X!!!!\n", curRecord->formID);
+                    curRecord->IsLoaded(false); //just incase the chosen flags were in use, clear them
+
+                    //Read (if FullLoad), no-op otherwise
+                    parser.Accept(curRecord);
+                    //Save any deleted records for post-processing
+                    if (curRecord->IsDeleted())
+                        DeletedRecords.push_back(curRecord);
+                    //Index it for fast, random lookup
+                    indexer.Accept(curRecord);
+                }
+
+                records.clear();
+            }
+
+            return true;
+        }
+
+        uint32_t Write(FileWriter &writer, std::vector<FormIDResolver *> &Expanders, FormIDResolver &expander, FormIDResolver &collapser, const bool &bMastersChanged, bool CloseMod)
+        {
+            std::vector<Record *> Records;
+            pool.MakeRecordsVector(Records);
+            uint32_t numRecords = (uint32_t)Records.size();
+            if (numRecords == 0)
+                return 0;
+
+            uint32_t type = REV32(GRUP);
+            uint32_t gType = eTop;
+            uint32_t TopSize = 0;
+            uint32_t TopLabel = RecType;
+            uint32_t formCount = 0;
+
+            //Top GRUP Header
+            writer.file_write(&type, 4);
+            uint32_t TopSizePos = writer.file_tell();
+            writer.file_write(&TopSize, 4); //Placeholder: will be overwritten with correct value later.
+            writer.file_write(&TopLabel, 4);
+            writer.file_write(&gType, 4);
+            writer.file_write(&stamp, 4);
+            writer.file_write(&unknown, 4);
+            ++formCount;
+            TopSize = 24;
+
+            formCount += numRecords;
+            for(uint32_t p = 0; p < numRecords; p++)
+                TopSize += Records[p]->Write(writer, bMastersChanged, expander, collapser, Expanders);
+
+            writer.file_write(TopSizePos, &TopSize, 4);
+            if (CloseMod)
+                pool.purge_with_destructors();
+
+            return formCount;
+        }
+    };
 
 template<uint32_t RecType, uint32_t AllocUnit, bool IsKeyedByEditorID>
 class TES5GRUPRecords<Sk::CELLRecord, RecType, AllocUnit, IsKeyedByEditorID>
