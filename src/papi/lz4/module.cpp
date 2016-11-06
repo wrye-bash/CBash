3078f519aa4a633c62746aa4bf1a86a5225e06ce
leandor <leandor@gmail.com>
2016-11-06 01:49:52 -0300

    (#18) Better error messages
    Included boost::format for generating formatted strings in the
    exception messages.

--- src/papi/lz4/module.cpp
+++ src/papi/lz4/module.cpp
@@ -2,11 +2,14 @@
 
 #include "module.h"
 
+#include <boost/format.hpp>
+
 #include <gsl/gsl>
 #include <string>
 #include <vector>
 #include <tuple>
 
+using boost::format;
 
 py::bytes lz4::compress(const py::bytes& buffer) 
 {
@@ -18,8 +21,9 @@ py::bytes lz4::compress(const py::bytes& buffer)
 
     auto res = api::compress_default(source, dest);
 
-    if (res == 0) {
-        throw std::exception("compression failed");
+    if (res <= 0) {
+        const auto errorMsg = (format("LZ4: compression failed with result code: %1%") % res).str();
+        throw std::exception(errorMsg.c_str());
     }
 
     return py::bytes(dest.data(), res);
